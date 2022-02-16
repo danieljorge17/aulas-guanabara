8f085b3461648e5dbc96db5980b120dcd8896b67
danieljorge17 <danieljorge17@hotmail.com>
2022-02-16 11:00:35 -0300

    Criado a resolução do exercício 4 e do rascunho 2

---
+++ exerc_04.py
@@ -0,0 +1,61 @@
+#Exercício 03
+
+var_01 = input('Insira algo: ')
+
+print('O tipo do que você digitou é {}'.format(type(var_01)))
+
+if var_01.isalpha():
+    print('O que você digitou é alfabético')
+# Uma string alfabética é uma string que contém apenas as letras
+# presentes no alfabeto. Espaços, números e  símbolos não são 
+# permitidos. Nota: 'ª' e 'º' são considerados alfabéticos
+# para esse método.
+
+if var_01.isalnum():
+    print('O que você digitou é alfanumérico')
+# Uma string alfanumérica é uma string que contém apenas as
+# letras presentes no alfabeto e números de 0 a 9. Espaços e
+# símbolos são não permitidos. Nota: 'ª' e 'º' são considerados
+# alfabéticos para esse método.
+
+if var_01.isupper():
+    print('O que você digitou está tudo em maiúsculo')
+# Nota: 'ª' e 'º' são considerados caracteres minúsculos para
+# esse método.
+
+if var_01.islower():
+    print('O que você digitou está tudo em minúsculo')
+# Nota: 'ª' e 'º' são considerados caracteres minúsculos para
+# esse método.
+    
+if var_01.isnumeric():
+    print('O que você digitou está tudo em numérico')
+
+if var_01.isdigit():
+    print('O que você digitou são apenas dígitos')
+
+if var_01.isdecimal():
+    print('O que você digitou é um número decimal')
+
+if var_01.isascii():
+    print('O que você digitou está presente na tabela ASCII')
+
+if var_01.isidentifier():
+    print('O que você digitou é um identificador')
+# Identificador é uma string que contém apenas letras, números
+# ou underlines e não podendo começar com um número ou conter
+# espaços.
+
+if var_01.isprintable():
+    print('Todos os caracteres você digitou são imprimíveis')
+# Alguns caracteres não são imprimíveis. Exemplo: '\n'.
+
+if var_01.isspace():
+    print('Todos os caracteres são espaços')
+
+if var_01.istitle():
+    print('O que você digitou é um título')
+# Título são strings que contém palavras começadas em maíusculo
+# e o resto dos caracteres da palavra em minúsculo. São ignorados
+# símbolos não alfabéticos. Nota: 'ª' e 'º' são considerados
+# caracteres minúsculos para esse método.
---
+++ rasc_02.py
@@ -0,0 +1,40 @@
+# Rascunhos 02 - Brincando Escape Codes
+
+print('Uma frase\n qualquer')
+# Quebra de linha
+
+print('Uma frase\t qualquer')
+# Tabulação horizontal
+
+print('Uma frase\v qualquer')
+# Tabulação vertical
+
+print('Uma frase\b qualquer')
+# Apaga o último caractere
+
+print('Uma frase\a qualquer')
+# ASCII Bell (BEL)
+
+print('Uma frase\r qualquer')
+# ASCII Carriage Return (CR)
+
+print('Uma frase\f qualquer')
+# ASCII Formfeed (FF)
+
+
+# '\\' -> \
+
+# '\\'' -> '
+
+# '\"' -> "
+
+# '\o00' -> Caractere com valor octal de '00'
+
+# '\x00' -> Caractere com valor hexadecimal de '00'
+
+# '\u0000' -> Caractere com valor hexadecimal 16-bit de '0000'
+
+# '\U00000000' -> Caractere com valor hexadecimal 16-bit
+# de '00000000'
+
+# \N{name} -> Caractere com nome 'name' na tabela Unicode
