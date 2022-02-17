b3ea1ed77d6a6d8b41eec45f9b99dae4859d3c8b
danieljorge17 <danieljorge17@hotmail.com>
2022-02-17 08:20:27 -0300

    Criado a resolução do sexto exercício

---
+++ exerc_06.py
@@ -0,0 +1,9 @@
+#Exercício 06
+
+var_01 = int(input('Insira um número: '))
+
+print('O dobro desse número é {},'.format(var_01*2), end=' ')
+
+print('o triplo é {},'.format(var_01*3), end=' ')
+
+print('e a raiz quadrada é {}'.format(var_01**(1/2)))
