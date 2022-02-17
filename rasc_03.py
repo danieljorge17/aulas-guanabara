e9617f2d8d6badc23900f52f460bd3cf16672e71
danieljorge17 <danieljorge17@hotmail.com>
2022-02-17 08:05:30 -0300

    Adicionado mais alguns prints ao rascunho 03

--- rasc_03.py
+++ rasc_03.py
@@ -24,8 +24,12 @@ print('Uma frase {0}{1}'.format(var_01, var_02))
 
 print('Uma frase {1}{0}'.format(var_01, var_02))
 
-print('Uma frase {var_01}{var_02}'.format(var_01=var_01, var_02=var_02))
+print('Uma frase {var_01}{var_02}'.format(var_01 = var_01, var_02 = var_02))
 
-print('Uma frase {var_1}{var_2}'.format(var_1=var_01, var_2=var_02))
+print('Uma frase {var_1}{var_2}'.format(var_1 = var_01, var_2 = var_02))
 
-print('Uma frase {var_1}{var_2}'.format(var_2=var_01, var_1=var_02))
+print('Uma frase {var_1}{var_2}'.format(var_2 = var_01, var_1 = var_02))
+
+print('Uma frase {}'.format(var_01), end = var_02)
+
+print('Uma frase {}'.format(var_01), end = var_02 + '\n')
